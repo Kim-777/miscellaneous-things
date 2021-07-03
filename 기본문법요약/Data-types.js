23b3f0e151cf876401303f958aad7bb24d403ab3
Kim-777 <iloo3012@naver.com>
2021-07-03 09:44:32 +0900

    ladash

--- 기본문법요약/Data-types.js
+++ 기본문법요약/Data-types.js
@@ -86,3 +86,6 @@ console.log(keyObject);
 console.log(map.has(keyObject));
 map.delete(keyObject);
 console.log(map);   // map이라도 하자 오느류ㅠㅠㅠㅠㅠㅠ
+map.clear();
+
+//  map[key] = 2와 같이 사용할 수 있지만 이 방법으로 값을 넣을 시 일반 객체처럼 취급하게 됨. 그러므로 꼭 set, get 메서드 사용합시당!
