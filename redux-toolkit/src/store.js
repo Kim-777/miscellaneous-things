0d937afb0b7169bd33ae050fb761726997408c36
김예찬 <iloo3012@naver.com>
2021-08-09 09:04:52 +0900

    hi

--- redux-toolkit/src/store.js
+++ redux-toolkit/src/store.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 // Or from '@reduxjs/toolkit/query/react'
 import { setupListeners } from '@reduxjs/toolkit/query'
 import { pokemonApi } from './services/pokemon'
+import logger from 'redux-logger'
 
 export const store = configureStore({
   reducer: {
@@ -11,9 +12,9 @@ export const store = configureStore({
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(pokemonApi.middleware),
+    getDefaultMiddleware().concat(pokemonApi.middleware).concat(logger),
 })
-// 뭐여
+
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch)
